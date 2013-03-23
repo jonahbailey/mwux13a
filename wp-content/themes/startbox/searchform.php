9ada779654c9e6da83a0deb2a3de44a80b396356
Jonah Bailey <jonah@mutuallyhuman.com>
2013-03-23 11:18:02 -0400

    start box is a mess, no thanks.

--- wp-content/themes/startbox/searchform.php
+++
@@ -1,6 +0,0 @@
-<form class="searchform" method="get" action="<?php echo esc_url( home_url() ); ?>">
-	<div>
-		<input name="s" type="text" class="searchtext" value="" title="<?php echo esc_attr( apply_filters( 'sb_search_text', 'Search' ) ); ?>" size="10" tabindex="1" />
-		<input type="submit" class="searchbutton button" value="Search" tabindex="2" />
-	</div>
-</form>
